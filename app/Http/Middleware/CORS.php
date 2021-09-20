c9ca56ab9c1e6652a1aacda3dacdb62dcbe66673
Narhzih <omosekejiolawale@gmail.com>
2021-09-20 18:23:46 +0100

    Removed custom CORS

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -16,23 +16,23 @@ class CORS
      */
     public function handle(Request $request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Origin: https://pennee-ui.netlify.app');
-        header('Access-Control-Allow-Origin: http://localhost:8080');
-        // ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
-        ];
-        if($request->getMethod() == "OPTIONS") {
-            return response()->json('OK', 200, $headers);
-        }
+//        header('Access-Control-Allow-Origin: *');
+//        header('Access-Control-Allow-Origin: https://pennee-ui.netlify.app');
+//        header('Access-Control-Allow-Origin: http://localhost:8080');
+//        // ALLOW OPTIONS METHOD
+//        $headers = [
+//            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+//            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
+//        ];
+//        if($request->getMethod() == "OPTIONS") {
+//            return response()->json('OK', 200, $headers);
+//        }
 
         $response = $next($request);
 
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
+//        foreach ($headers as $key => $value) {
+//            $response->header($key, $value);
+//        }
         return $response;
     }
 }
