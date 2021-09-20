dc2b9e78f66266329823b0c8a89db18c55c45cb6
Narhzih <omosekejiolawale@gmail.com>
2021-09-20 14:57:00 +0100

    Added database seed files

--- database/seeders/FaqsTableSeeder.php
+++ database/seeders/FaqsTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class FaqsTableSeeder extends Seeder
 {
@@ -13,6 +14,32 @@ class FaqsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $inputs = [
+            [
+                'question' => 'What is Pennee',
+                'answer' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur doloribus eaque earum eius eum fuga harum ipsa modi nihil officiis saepe sequi similique, vero. Amet eum modi mollitia vel vitae.'
+            ],
+            [
+                'question' => 'How much does it cost to maintain a pennee account',
+                'answer' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur doloribus eaque earum eius eum fuga harum ipsa modi nihil officiis saepe sequi similique, vero. Amet eum modi mollitia vel vitae.'
+            ],
+            [
+                'question' => 'What are asset loans',
+                'answer' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur doloribus eaque earum eius eum fuga harum ipsa modi nihil officiis saepe sequi similique, vero. Amet eum modi mollitia vel vitae.'
+            ],
+            [
+                'question' => 'What do overdrafts cover',
+                'answer' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur doloribus eaque earum eius eum fuga harum ipsa modi nihil officiis saepe sequi similique, vero. Amet eum modi mollitia vel vitae.'
+            ]
+        ];
+
+        foreach ($inputs as $input) {
+            DB::table('faqs')->insert([
+               'question' => $input['question'],
+               'answer' => $input['answer'],
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
     }
 }
